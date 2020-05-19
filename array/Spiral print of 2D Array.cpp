72fa731d6b0fdaff9dfd14eb9f05d16c0131dc13
praveenkumarsrivas <pks101295@gmail.com>
2020-05-19 12:44:06 +0530

    array item

--- array/Spiral print of 2D Array.cpp
+++ array/Spiral print of 2D Array.cpp
@@ -1,60 +1,59 @@
-#include <iostream>
-using namespace std; 
-void Spiral_Print(int arr[][100],int m,int n)
+#include <bits/stdc++.h>
+using namespace std;
+void Spiral_Print(int arr[][100], int m, int n)
 {
-    int Sr=0,Er=m-1,Sc=0,Ec=n-1;
-    while(Sr<=Er && Sc<=Ec)
-    {        
-         //start row
-        for(int i=Sr;i<=Ec;i++)       
-        cout<<arr[Sr][i]<<" ";
+    int Sr = 0, Er = m - 1, Sc = 0, Ec = n - 1;
+    while (Sr <= Er && Sc <= Ec)
+    {
+        //start row
+        for (int i = Sr; i <= Ec; i++)
+            cout << arr[Sr][i] << " ";
         Sr++;
 
         //end col
-        for(int i=Sr;i<=Er;i++)
-        cout<<arr[i][Ec]<<" ";
+        for (int i = Sr; i <= Er; i++)
+            cout << arr[i][Ec] << " ";
         Ec--;
         //end-bottom row
-        if(Er>Sr)
+        if (Er > Sr)
         {
-        for(int i=Ec;i>=Sc;i--)
-        cout<<arr[Er][i]<<" ";
-        Er--;
+            for (int i = Ec; i >= Sc; i--)
+                cout << arr[Er][i] << " ";
+            Er--;
         }
 
         //start col
-        if(Ec>Sc)
+        if (Ec > Sc)
         {
-        for(int i=Er;i>=Sr;i--)
-        cout<<arr[i][Sc]<<" ";
-        Sc++;
+            for (int i = Er; i >= Sr; i--)
+                cout << arr[i][Sc] << " ";
+            Sc++;
         }
     }
-
 }
-int main() {
-    int m,n;
-    cin>>m>>n;
-    int arr[100][100],x=1;
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    int arr[100][100], x = 1;
     //GENERATING MATRIX
-    for(int i=0;i<m;i++)
+    for (int i = 0; i < m; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            arr[i][j]=x;
+            arr[i][j] = x;
             x++;
         }
     }
-    for(int i=0;i<m;i++)
+    for (int i = 0; i < m; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            cout<<arr[i][j]<<" ";
-            
-        }cout<<endl;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
-    Spiral_Print(arr,m,n);
-
+    Spiral_Print(arr, m, n);
 
     return 0;
 }
