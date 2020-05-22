06e8c8cae076b871303c4a600a08ee7728847813
praveenkumarsrivas <pks101295@gmail.com>
2020-05-22 08:16:25 +0530

    string

--- Mathematics/sieve_prime.cpp
+++ Mathematics/sieve_prime.cpp
@@ -1,42 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define MAX 1000000
 typedef unsigned long ll;
 using namespace std;
-void sieve_prime(ll *arr,ll n){
+void sieve_prime(bool *arr, ll n)
+{
 	//first marking all odd as prime number
-	for(ll i=3;i<=MAX;i+=2)
-	   arr[i]=1;
- 
+	for (ll i = 3; i <= MAX; i += 2)
+		arr[i] = 1;
+
 	//applying sieve
-	for(ll i=3;i<=MAX;i+=2){
-		if(arr[i]==1){
-			for(ll j=i*i;j<=MAX;j+=i){
-				arr[j]=0;
+	for (ll i = 3; i <= MAX; i += 2)
+	{
+		if (arr[i] == 1)
+		{
+			for (ll j = i * i; j <= MAX; j += i)
+			{
+				arr[j] = 0;
 			}
 		}
 	}
 	//special case
-	arr[0]=0;
-	arr[1]=0;
-	arr[2]=1;
- 
-	int res=0,i=0;
-    while(n>0){
-		if(arr[i]==1){
-		    res=i;
-		    //cout<<i<<" ";
-		    n--;
+	arr[0] = 0;
+	arr[1] = 0;
+	arr[2] = 1;
+
+	int res = 0, i = 0;
+	while (n > 0)
+	{
+		if (arr[i] == 1)
+		{
+			res = i;
+			//cout<<i<<" ";
+			n--;
 		}
 		i++;
 	}
-	cout<<res<<endl;
+	cout << res << endl;
 }
-int main() {
+int main()
+{
 	ll n;
-	cin>>n;
-	ll arr[MAX]={0};
-	sieve_prime(arr,n);
- 
+	cin >> n;
+	bool arr[MAX] = {0};
+	sieve_prime(arr, n);
+
 	return 0;
 }
 
