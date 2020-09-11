042ac35af57f4f38b9f23c941bc5f727a2b45eff
praveenkumarsrivas <pks101295@gmail.com>
2020-09-11 15:55:35 +0530

    commit for searching recursive in LL

--- linkedlist/searching_recursive.cpp
+++ linkedlist/searching_recursive.cpp
@@ -13,9 +13,10 @@ public:
 };
 void create_list(Node *&head, int d)
 {
-    if(head == NULL)
+    if (head == NULL)
     {
-        head=new Node(d);
+        head = new Node(d);
+        //head = n;
     }
     else
     {
@@ -23,10 +24,41 @@ void create_list(Node *&head, int d)
         n->next = head;
         head = n;
     }
+    return;
 }
-bool 
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " --> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+bool searching_recursive(Node *head, int key)
+{
+    if (head == NULL)
+        return false;
+    if (head->data == key)
+        return true;
+    return searching_recursive(head->next, key);
+}
 int main()
 {
+    Node *head = NULL;
+    create_list(head, 1);
+    create_list(head, 2);
+    create_list(head, 3);
+    create_list(head, 4);
+    print(head);
+    cout << "Enter key to search in the list: >";
+    int key;
+    cin >> key;
+    if (searching_recursive(head, key))
+        cout << "Element Found!" << endl;
+    else
+        cout << "Element Not Found!" << endl;
 
     return 0;
 }
+
